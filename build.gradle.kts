6e53f848486a188cc749b1c3dfe84051476544ae
Pavel Annin <anninpavel2014@gmail.com>
2023-10-03 19:09:13 +0300

    add publish script to maven central

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,5 +4,18 @@ plugins {
     alias(deps.plugins.android.library) apply false
     alias(deps.plugins.kotlin.android) apply false
     alias(deps.plugins.kotlin.jvm) apply false
+    alias(deps.plugins.publish) apply true
 }
 true // Needed to make the Suppress annotation work for the plugins block
+
+nexusPublishing {
+    repositories {
+        sonatype {
+            nexusUrl.set(uri("https://s01.oss.sonatype.org/service/local/"))
+            snapshotRepositoryUrl.set(uri("https://s01.oss.sonatype.org/content/repositories/snapshots/"))
+            stagingProfileId.set(property("sonatypeStagingProfileId").toString())
+            username.set(project.property("ossrhUsername").toString())
+            password.set(project.property("ossrhPassword").toString())
+        }
+    }
+}
--- vexillum/build.gradle.kts
+++ vexillum/build.gradle.kts
@@ -1,14 +1,63 @@
 @Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove once KTIJ-19369 is fixed
 plugins {
     alias(deps.plugins.kotlin.jvm)
+    `maven-publish`
+    signing
 }
 
-group = "io.github.pavelannin"
-version = "1.0.0"
+val PUBLISH_GROUP_ID = "io.github.pavelannin"
+val PUBLISH_ARTIFACT_ID = "vexillum"
+val PUBLISH_VERSION = "0.1.0"
+
+group = PUBLISH_GROUP_ID
+version = PUBLISH_VERSION
 
 java {
     sourceCompatibility = JavaVersion.VERSION_11
     targetCompatibility = JavaVersion.VERSION_11
+}
+
+afterEvaluate {
+    publishing {
+        publications {
+            create<MavenPublication>("mavenJava") {
+                groupId = PUBLISH_GROUP_ID
+                artifactId = PUBLISH_ARTIFACT_ID
+                version = PUBLISH_VERSION
+                from(project.components.getByName("java"))
+
+                java.withSourcesJar()
+                java.withJavadocJar()
+
+                pom {
+                    name.set(PUBLISH_ARTIFACT_ID)
+                    description.set("Android feature toggling")
+                    url.set("https://github.com/pavelannin/android-vexillum")
+                    licenses {
+                        license {
+                            name.set("MIT License")
+                            url.set("http://www.opensource.org/licenses/mit-license.php")
+                        }
+                    }
+                    developers {
+                        developer {
+                            name.set("Pavel Annin")
+                            email.set("pavelannin.dev@gmail.com")
+                        }
+                    }
+                    scm {
+                        connection.set("scm:git:github.com/pavelannin/android-vexillum.git")
+                        developerConnection.set("scm:git:ssh://github.com/pavelannin/android-vexillum.git")
+                        url.set("https://github.com/pavelannin/android-vexillum/tree/main")
+                    }
+                }
+            }
+        }
+    }
+}
+
+signing {
+    sign(publishing.publications)
 }
 
 dependencies {
